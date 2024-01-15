9efccf0f098de25270b7ead49d8ea06e1ce0721f
xianzhan <424447832@qq.com>
2024-01-15 23:39:35 +0800

    update: main.rs 清理代码

--- src/main/rust/src/main.rs
+++ src/main/rust/src/main.rs
@@ -1,8 +1,10 @@
+use std::process;
+
 use windows::{
     core::PCWSTR,
     Win32::{
         Foundation::BOOL,
-        System::Threading::{OpenProcess, PROCESS_ACCESS_RIGHTS},
+        System::Threading::{OpenProcess, PROCESS_ALL_ACCESS},
         UI::WindowsAndMessaging::{FindWindowW, GetWindowThreadProcessId},
     },
 };
@@ -13,27 +15,19 @@ fn main() {
             PCWSTR::null(),
             PCWSTR::from_raw("扫雷\0".encode_utf16().collect::<Vec<u16>>().as_ptr()),
         );
-        println!("winmine HWND: {:?}", winmine);
 
         let mut pid = 0u32;
-        let thread_id = GetWindowThreadProcessId(winmine, Some(&mut pid));
-        println!("pid: {pid}, thread_id: {thread_id}");
+        let _ = GetWindowThreadProcessId(winmine, Some(&mut pid));
         if pid == 0 {
-            println!("扫雷未打开，退出程序.");
-            return;
+            eprintln!("扫雷获取 pid 失败, 退出助手");
+            process::exit(-1);
         }
+        println!("扫雷程序已启动");
 
-        let p_handle = OpenProcess(
-            PROCESS_ACCESS_RIGHTS(0x000F_0000 | 0x0010_0000 | 0xFFFF),
-            BOOL(0),
-            pid,
-        );
-
-        match p_handle {
-            Err(e) => println!("打开进程错误. {}", e),
-            Ok(handle) => {
-                println!("打开进程成功. {:?}", handle)
-            }
+        let p_handle = OpenProcess(PROCESS_ALL_ACCESS, BOOL(0), pid);
+        if let Err(e) = p_handle {
+            eprintln!("扫雷 HANDLE 获取失败, 退出助手. {}", e);
+            process::exit(-2);
         }
     }
 }
